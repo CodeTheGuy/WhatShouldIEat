c2d70648d1536fedc8fa4dd74a8a1093c3728799
Cody Stricker <codystri@iastate.edu>
2020-02-05 13:17:01 -0600

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
+import Recipe from "./Recipe";
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+
+  //These are taken from the edamam API
+  const APP_ID = "d6555000";
+  const APP_KEY = "7cb56f715642c34bb6ba13ef7fbb23a1";
+
+  //Contructors, recipes 
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState('Chicken')
+  useEffect(() =>{
+    getRecipes();
+  }, [query]);
+
+  //Gets recipe information
+  const getRecipes = async () => {
+      const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+      const data = await response.json();
+      setRecipes(data.hits);
+  }
+
+  //A gate that allows the target search to go through after search has been clicked
+  const updateSearch = e => {
+    setSearch(e.target.value);
+    console.log(search);
+  }
+
+  //Search bar UI, allows the search bar to be clicked to input information
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
+
+  //Creates all the front end information by fetching info from the recipe API
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="recipes">
+      {recipes.map(recipe =>(
+        <Recipe
+        key={recipe.recipe.label}
+        title={recipe.recipe.label} 
+        calories={recipe.recipe.calories} 
+        image={recipe.recipe.image}
+        ingredients={recipe.recipe.ingredients}
+        />
+      ))}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
