a84135487da5f4ef18e63a28083be7aebe7da4fc
ryan8116 <55749812+ryan8116@users.noreply.github.com>
2020-03-04 21:23:11 -0600

    added a lot to about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,64 @@
 import React from "react";
-import State from "./state.png";
+import "../../App.css";
+
 const About = () => (
   <div>
     <h1>
       <center>A Little Bit About Us</center>
     </h1>
+
     <b1>
-      <center></center>
+      <center>
+        <img src="/images/skyline1.jpg" alt="" class="image2" />
+      </center>
+    </b1>
+    <h2>
+      <center>Our Mission:</center>
+    </h2>
+    <b1>
+      <center>
+        To solve the ever growing problem of not knowing what you want to eat.{" "}
+      </center>
+      <center>
+        There is no worse feeling than being hungry and not knowing what to
+        make. This global issue
+      </center>
+      <center>
+        is stunting effecincy and productivity in all aspects of life. We
+        believe our product will
+      </center>
+      <center>
+        help make the world a better place now and for generations to come.
+      </center>
+    </b1>
+    <b1>
+      <center>
+        <img src="/images/state2.png" alt="" class="image1" />
+      </center>
+    </b1>
+
+    <b1>
+      <h2>
+        <center>Web Development Team:</center>
+      </h2>
+      <h3>
+        <center>Ryan Scehovic</center>
+      </h3>
+      <h3>
+        <center>Ryan McNally</center>
+      </h3>
+      <h3>
+        <center>Ryan Hickhok</center>
+      </h3>
+      <h3>
+        <center>Cody Stricker</center>
+      </h3>
+    </b1>
+
+    <b1>
+      <center>
+        <img src="/images/state3.png" alt="" class="image1" />
+      </center>
     </b1>
   </div>
 );
