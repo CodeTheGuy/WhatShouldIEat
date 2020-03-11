267471dfae0b816469518b7ae4524b817bc8f442
Ryan McNally <rmcnally@iastate.edu>
2020-03-11 10:16:17 -0500

    lots of firebase stuff, hope this works

---
+++ build/precache-manifest.79bb6c632045a14e5f48608af07be31f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f3567f98fb006fbfab5f8b35ff1662",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3628628393128ea3642",
+    "url": "/static/css/main.c042f213.chunk.css"
+  },
+  {
+    "revision": "54f857496011f750f00a",
+    "url": "/static/js/2.7b6809c7.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.7b6809c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3628628393128ea3642",
+    "url": "/static/js/main.dcc05c71.chunk.js"
+  },
+  {
+    "revision": "fb940f57727f90c646cf",
+    "url": "/static/js/runtime-main.5e6a58ed.js"
+  }
+]);
