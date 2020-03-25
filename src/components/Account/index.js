2776e2bd596a8c8e31c8dcb261dadc29e6251e12
Ryan McNally <rmcnally@iastate.edu>
2020-03-25 10:19:56 -0500

    minor changes to the Account page

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,7 +1,28 @@
-import React from 'react';
-const Account = () => (
-  <div>
-    <h1>Account</h1>
-  </div>
-);
+import React, { useState } from "react";
+import GoogleLogin from "react-google-login";
+
+const Account = () => {
+  const [name, setName] = useState("");
+
+  const [email, setEmail] = useState("");
+
+  const [url, setUrl] = useState("");
+
+  const responseGoogle = response => {
+    setName(response.profileObj.name);
+    setEmail(response.profileObj.email);
+    setUrl(response.profileObj.imageUrl);
+  };
+
+  return (
+    <div>
+      <center>
+        <h1>Account Information</h1>
+        <h3>Welcome {name}</h3>
+        <h3>Email: {email}</h3>
+        <image src={url} alt={name} />
+      </center>
+    </div>
+  );
+};
 export default Account;
