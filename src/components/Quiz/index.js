b92c464127579111f6a8168982c552476346f716
ryan8116 <55749812+ryan8116@users.noreply.github.com>
2020-04-22 17:46:16 -0500

    basic quiz/survey function

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,17 +1,112 @@
-import React, { Component } from 'react';
-import {Link} from 'react-router-dom'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "survey-react/survey.css";
+import * as Survey from "survey-react";
 
-class Quiz extends Component{
-
-  render(){
-    return (
-    <div className="Quiz">
-      <h2>
-        <center>WORK IN PROGRESS</center>
-      </h2>
-    </div>
-    );
+class Quiz extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.onCompleteComponent = this.onCompleteComponent.bind(this);
+  }
+  onCompleteComponent = () => {
+    this.setState({
+      isComplted: true,
+    });
   };
+  render() {
+    var json = {
+      completedHtml:
+        "<h3>Thank you for your feedback.</h3> <h5>Your thoughts and ideas will help us to create a great product!</h5>",
+      completedHtmlOnCondition: [
+        {
+          expression: "{nps_score} > 8",
+          html:
+            "<h3>Thank you for your feedback.</h3> <h5>We glad that you love our product. Your ideas and suggestions will help us to make our product even better!</h5>",
+        },
+        {
+          expression: "{nps_score} < 7",
+          html:
+            "<h3>Thank you for your feedback.</h3> <h5> We are glad that you share with us your ideas.We highly value all suggestions from our customers. We do our best to improve the product and reach your expectation.</h5>\n",
+        },
+      ],
+      pages: [
+        {
+          name: "page1",
+          elements: [
+            {
+              type: "rating",
+              name: "nps_score",
+              title:
+                "On a scale of zero to ten, how likely are you to recommend our product to a friend or colleague?",
+              isRequired: true,
+              rateMin: 0,
+              rateMax: 10,
+              minRateDescription: "(Most unlikely)",
+              maxRateDescription: "(Most likely)",
+            },
+            {
+              type: "checkbox",
+              name: "promoter_features",
+              visibleIf: "{nps_score} >= 9",
+              title: "What features do you value the most?",
+              isRequired: true,
+              validators: [
+                {
+                  type: "answercount",
+                  text: "Please select two features maximum.",
+                  maxCount: 2,
+                },
+              ],
+              hasOther: true,
+              choices: [
+                "Performance",
+                "Stability",
+                "User Interface",
+                "Complete Functionality",
+              ],
+              otherText: "Other feature:",
+              colCount: 2,
+            },
+            {
+              type: "comment",
+              name: "passive_experience",
+              visibleIf: "{nps_score} > 6  and {nps_score} < 9",
+              title: "What is the primary reason for your score?",
+            },
+            {
+              type: "comment",
+              name: "disappointed_experience",
+              visibleIf: "{nps_score} notempty",
+              title:
+                "What do you miss and what was disappointing in your experience with us?",
+            },
+          ],
+        },
+      ],
+      showQuestionNumbers: "off",
+    };
+    var surveyRender = !this.state.isComplted ? (
+      <Survey.Survey
+        json={json}
+        showCompletedPage={false}
+        onComplte={this.onCompleteComponent}
+      />
+    ) : null;
+
+    var onSurveyCompletion = this.state.isComplted ? (
+      <div>Thanks for completeing the survey</div>
+    ) : null;
+
+    return (
+      <div className="Quiz">
+        <div>
+          {surveyRender}
+          {onSurveyCompletion}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Quiz;
